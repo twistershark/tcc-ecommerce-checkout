ef6837ae0ab80ce2b5d016dcde9404bf9474b26a
Paulo Victor <paulovmel@gmail.com>
2023-11-26 15:21:29 -0300

    feat: updates http client adapter interface

--- src/infrastructure/adapters/http-client-adapter-interface.ts
+++ src/infrastructure/adapters/http-client-adapter-interface.ts
@@ -1,4 +1,6 @@
+import { Address } from "../../domain/object-values/address";
+
 export interface HttpClientAdapterInterface {
-  get: (pathname: string) => Promise<any>;
+  get: (pathname: string) => Promise<Address>;
   post: (pathname: string, payload: any) => Promise<boolean>;
 }
