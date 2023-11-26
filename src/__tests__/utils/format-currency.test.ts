2c5908df205b9223663a02131b3091ca05c70e40
Paulo Victor <paulovmel@gmail.com>
2023-11-26 16:44:29 -0300

    test: adds new test case to format currency test

--- src/__tests__/utils/format-currency.test.ts
+++ src/__tests__/utils/format-currency.test.ts
@@ -7,4 +7,11 @@ describe("formatCurrency", () => {
 
     expect(formattedValue!.replace(/\s+/g, " ")).toEqual("R$ 1.000,00");
   });
+
+  it("should return null if no value if provided", () => {
+    const value = undefined;
+    const formattedValue = formatCurrency(value!);
+
+    expect(formattedValue).toEqual(null);
+  });
 });
