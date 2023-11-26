6dbc4bf635af1f073cbc0d150f4c8ab2dfd21361
Paulo Victor <paulovmel@gmail.com>
2023-11-26 16:00:35 -0300

    test: creates mock order repository

---
+++ src/__tests__/__mocks__/infrastructure/repositories/mock-order-repository.ts
@@ -0,0 +1,20 @@
+import { Address } from "../../../../domain/object-values/address";
+import { OrderRepositoryInterface } from "../../../../domain/repositories/order-repository-interface";
+import { CreateOrderDTO } from "../../../../presentation/dtos/create-order-dto";
+
+export class MockOrderRepository implements OrderRepositoryInterface {
+  createOrder(_payload: CreateOrderDTO): Promise<boolean> {
+    return Promise.resolve(true);
+  }
+
+  async getAddressByCEP(_cep: string) {
+    const response: Address = {
+      street: "Praça da Sé",
+      neighborhood: "Sé",
+      city: "São Paulo",
+      state: "SP",
+    };
+
+    return Promise.resolve(response);
+  }
+}
