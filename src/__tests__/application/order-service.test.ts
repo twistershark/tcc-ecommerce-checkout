f7e560b07f1845ea003e69745024d4b4672f7b6b
Paulo Victor <paulovmel@gmail.com>
2023-11-26 16:00:09 -0300

    test: creates order-service.test.ts

---
+++ src/__tests__/application/order-service.test.ts
@@ -0,0 +1,45 @@
+import { OrderService } from "../../application/services/order-service";
+import { Address } from "../../domain/object-values/address";
+import { CreateOrderDTO } from "../../presentation/dtos/create-order-dto";
+import { MockOrderRepository } from "../__mocks__/infrastructure/repositories/mock-order-repository";
+
+describe("OrderService", () => {
+  const orderService = new OrderService(new MockOrderRepository());
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should create an order successfully", async () => {
+    const createOrderDTO: CreateOrderDTO = {
+      name: "Test name",
+      lastname: "Test last",
+      email: "email@email.com",
+      cep: "00000-000",
+      address: "Rua joão de prad",
+      number: "SN",
+      district: "Bairro",
+      city: "Luzicyty",
+      state: "GO",
+    };
+
+    const result = await orderService.createOrder(createOrderDTO);
+
+    expect(result).toBe(true);
+  });
+
+  it("should get address by CEP successfully", async () => {
+    const cep = "12345-678";
+
+    const mockAddress: Address = {
+      street: "Praça da Sé",
+      neighborhood: "Sé",
+      city: "São Paulo",
+      state: "SP",
+    };
+
+    const result = await orderService.getAddressByCEP(cep);
+
+    expect(result).toEqual(mockAddress);
+  });
+});
