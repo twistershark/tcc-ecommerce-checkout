8cf0093e6f3a2cb7999f4727b3d039196e56d8de
Paulo Victor <paulovmel@gmail.com>
2023-11-26 15:30:09 -0300

    feat: creates  create-order-dto

---
+++ src/presentation/dtos/create-order-dto.ts
@@ -0,0 +1,11 @@
+export interface CreateOrderDTO {
+  name: string;
+  lastname: string;
+  email: string;
+  cep: string;
+  address: string;
+  number: string;
+  district: string;
+  city: string;
+  state: string;
+}
