401a3be4b4dbbd32d1fe4e4e8f11b12534b6f8ce
Paulo Victor <paulovmel@gmail.com>
2023-11-27 19:55:49 -0300

    fix: removes redundant double negation

--- src/presentation/components/input/index.tsx
+++ src/presentation/components/input/index.tsx
@@ -19,10 +19,10 @@ export function Input({
         {...rest}
         className={[
           "ch-border ch-border-black ch-h-[62px] ch-px-4 disabled:ch-opacity-5",
-          !!errorMessage ? "ch-border-red-500" : "",
+          errorMessage ? "ch-border-red-500" : "",
         ].join(" ")}
       />
-      {!!errorMessage ? (
+      {errorMessage ? (
         <strong className="ch-text-red-500 ch-font-sans ch-text-sm -ch-mt-1">
           {errorMessage}
         </strong>
